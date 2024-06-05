03e100d3618256eded855d3525ee427cb6c2f9dd
Jay Brenneman <rjbrenn@us.ibm.com>
2024-06-05 10:22:46 -0400

    pep fixes psmapi , doc added zvm_dirm_nicdef

--- plugins/module_utils/psmapi.py
+++ plugins/module_utils/psmapi.py
@@ -19,15 +19,15 @@ import socket
 import ssl
 import sys
 
+
 def call_client(host, port, authuser, authpass, target, apicommand, *args):
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     context.load_default_certs()
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssock = context.wrap_socket(sock, server_hostname=host) 
+    ssock = context.wrap_socket(sock, server_hostname=host)
     server_address = (host, port)
     print("Connecting to server %s port %s" % server_address)
     ssock.connect(server_address)
-
 
     readback = 0
     numzeros = 0
@@ -37,86 +37,84 @@ def call_client(host, port, authuser, authpass, target, apicommand, *args):
     reason_code = 9999
     message_lines = ""
 
-    #print("num of args is %s" % len(args))
+    # print("num of args is %s" % len(args))
     if len(args) == 1:
         thecommand = args[0]
     if len(args) > 1:
         print("ERROR: What the heck more than 1 command string ?")
-        return(return_code)
+        return (return_code)
 
     print(thecommand)
-
 
     cmdstring = bytes()
     cmdbytes = bytes()
 
     print("Assembling the command string")
     cmdstring += len(apicommand).to_bytes(4, byteorder='big') + apicommand.encode('ASCII')
-    #print(cmdstring)
+    # print(cmdstring)
     cmdstring += len(authuser).to_bytes(4, byteorder='big') + authuser.encode('ASCII')
-    #print(cmdstring)
+    # print(cmdstring)
     cmdstring += len(authpass).to_bytes(4, byteorder='big') + authpass.encode('ASCII')
-    #be careful exposing the full cmdstring from this point on, it contains the unencrypted password
-    #print(cmdstring)
+    # be careful exposing the full cmdstring from this point on, it contains the unencrypted password
+    # print(cmdstring)
     cmdstring += len(target).to_bytes(4, byteorder='big') + target.encode('ASCII')
-    #print(cmdstring)
+    # print(cmdstring)
     cmdstring += thecommand.encode('ASCII')
-    #print(len(cmdstring))
-    #print(cmdstring)
+    # print(len(cmdstring))
+    # print(cmdstring)
     cmdbytes = len(cmdstring).to_bytes(4, byteorder='big') + cmdstring
-    #print(cmdbytes)
-
+    # print(cmdbytes)
 
     try:
-        #be careful with exposing the full cmdbytes because it contains the unencrypted password
-        #print("Sending %s" % cmdbytes)
+        # be careful with exposing the full cmdbytes because it contains the unencrypted password
+        # print("Sending %s" % cmdbytes)
         print("Sending the command string to SMAPI")
         ssock.sendall(cmdbytes)
         message = ""
-        while numzeros < 1: 
+        while numzeros < 1:
             print("Reading a response from SMAPI")
             reqnum = ssock.recv(4096)
             print("pulling off request number")
             request_num = int.from_bytes(reqnum, byteorder='big')
-            #print("raw reqnum: %s" % reqnum)
-            #print("int reqnum: %i" % request_num)
+            # print("raw reqnum: %s" % reqnum)
+            # print("int reqnum: %i" % request_num)
             print("pulling off full response")
             response = ssock.recv(4096)
-            #print("Response:")
-            #print(response)
-            #print("\n")
+            # print("Response:")
+            # print(response)
+            # print("\n")
             resp_length = int.from_bytes(response[0:4], byteorder='big')
-            #print(response[0:4])
-            #print("response totlen: %i" % resp_length)
+            # print(response[0:4])
+            # print("response totlen: %i" % resp_length)
             request_num = int.from_bytes(response[4:8], byteorder='big')
-            #print(response[4:8])
-            #print("response reqid: %i" % request_num)
+            # print(response[4:8])
+            # print("response reqid: %i" % request_num)
             return_code = int.from_bytes(response[8:12], byteorder='big')
-            #print(response[8:12])
-            #print("response rc: %i" % return_code)
+            # print(response[8:12])
+            # print("response rc: %i" % return_code)
             reason_code = int.from_bytes(response[12:16], byteorder='big')
-            #print(response[12:16])
-            #print("response rs: %i" % reason_code)
+            # print(response[12:16])
+            # print("response rs: %i" % reason_code)
             num_messages = int.from_bytes(response[16:20], byteorder='big')
-            #print("number of message lines: %i" % num_messages )
+            # print("number of message lines: %i" % num_messages )
 
-            #messages is a pointer into the response string, we will move it along
-            #the string to pull apart the individual message lines
+            # messages is a pointer into the response string, we will move it along
+            # the string to pull apart the individual message lines
             messages = response[20:]
             print("SMAPI Response contained VM command response messages, parsing")
             i = 0
             while i < num_messages:
-                #message format is <mesg_line_length><message>
-                #print("message left:")
-                #print(messages)
+                # message format is <mesg_line_length><message>
+                # print("message left:")
+                # print(messages)
                 line_len = int.from_bytes(messages[0:4], byteorder='big')
-                #print("line length: %i" % line_len)
+                # print("line length: %i" % line_len)
                 if line_len == 0:
                     break
                 stopchar = line_len + 4
-                #line = the message line up to the next message line length value
+                # line = the message line up to the next message line length value
                 line = messages[4:stopchar]
-                #print(">" + line.decode('ASCII') + "<")
+                # print(">" + line.decode('ASCII') + "<")
                 messages = messages[stopchar:]
                 message_lines += line.decode('ASCII') + "\n"
                 i += 1
@@ -131,8 +129,8 @@ def call_client(host, port, authuser, authpass, target, apicommand, *args):
         print("Closing connection")
         ssock.close()
 
-        results = []
-        results.append(return_code)
-        results.append(reason_code)
-        results.append(message_lines)
-        return(results)
+    results = []
+    results.append(return_code)
+    results.append(reason_code)
+    results.append(message_lines)
+    return (results)
--- plugins/modules/zvm_dirm_nicdef.py
+++ plugins/modules/zvm_dirm_nicdef.py
@@ -17,21 +17,124 @@ __metaclass__ = type
 
 
 DOCUMENTATION = r'''
+module: zvm_dirm_nicdef
+
+short_description: z/VM Dirmaint NICDEF command ansible module
+
+version_added: "0.0.3"
+
+description: Expose more of the Dirmaint NICDEF command syntax via SMAPI to permit ansible
+    based management of options such as setting a MAC address or VLAN tags in the directory
+
+options:
+    name:
+        description: this is the target who will have their NICDEF changed
+        required: true
+        type: str
+    zvmhost:
+        description: the hostname of the z/VM system where the target is running
+        required: true
+        type: str
+    port:
+        description: the TCP port number where SMAPI listens
+        required: true
+        type: int
+    authuser:
+        description: the z/VM user in VSMWORK1 AUTHLIST who is authorized to call this SMAPI function
+        required: true
+        type: str
+    authpass:
+        description: the z/VM users password for authuser
+        required: true
+        type: str
+    devno:
+        description: device number of the virtual network adapter
+        required: true
+        type: str
+    nictype:
+        description: device type - QDIO or HIPERsocket
+        required: false
+        choices:
+            - 'QDIO'
+            - 'HIPER'
+        default: 'HIPER'
+    lanname:
+        description: the name of the LAN or VSWITCH this virtual network adapter attaches to
+        required: false
+        type: str
+    lanowner:
+        description: the name of the LAN owning user if its a GuestLAN and is user owned.
+            SYSTEM is the default for all VSWITCHes and can also be correct for some GuestLANs
+        required: false
+        type: str
+        default: 'SYSTEM'
+    vlanid:
+        description: the VLAN ID string. It can be a single VLAN, a range, or a comma seperated list, or combination.
+        required: false
+        type: str
+    macid:
+        description: the last 6 characters of the MAC ID if using User Controlled MAC addresses
+        required: false
+        type: str
+    exists:
+        description: are we creating or deleting this NICDEV device?
+        required: false
+        type: bool
+        default: true
+
 author:
     - Jay Brenneman (@rjbrenn)
 '''
 
 
 EXAMPLES = r'''
+- name: connect to 9DOTLAN2
+  zvm_dirm_nicdef:
+    name: JTU001
+    zvmhost: 'lticvmc.example.net'
+    port: 44444
+    authuser: mapauth
+    authpass: '{{ mappassw }}'
+    devno: 888
+    nictype: QDIO
+    lanname: 9DOTLAN2
+    vlanid: 2230
+    exists: true
 '''
 
 
 RETURN = r'''
+return_code:
+    description: the integer return code from the SMAPI function.
+    returned: always
+    type: int
+    sample: 0
+reason_code:
+    description: the integer reason code from the SMAPI function.
+    returned: always
+    type: int
+    sample: 0
+return_stdout:
+    description: messages captured by the SMAPI function while running the requested command
+    returned: always
+    type: str
+    sample: "DVHELD1190I Command EXECLOAD complete; RC= 0.\n
+        Request number 20: DVHSAPI FOR JTU001 NICDEF 888 TYPE QDIO LAN SYSTEM 9DOTLAN2 V\n
+        LAN 2230 \n
+        resulted in the following 5 responses:\n
+        DVHXMT1191I Your NICDEF request has been sent for processing to DIRMAINT\n
+        DVHXMT1191I at LTICVM1 via DIRMSAT7.\n
+        DVHREQ2288I Your NICDEF request for JTU001 at * has been accepted.\n
+        DVHBIU3450I The source for directory entry JTU001 has been updated.\n
+        DVHBIU3425I The next ONLINE will take place as scheduled.\n
+        DVHREQ2289I Your NICDEF request for JTU001 at * has completed; with RC =\n
+        DVHREQ2289I 0.\n
+        DVHELD1190I Command EXECDROP complete; RC= 0.\n"
 '''
 
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.ibm.zvm_ansible.plugins.module_utils.psmapi import(call_client)
+from ansible_collections.ibm.zvm_ansible.plugins.module_utils.psmapi import (call_client)
 
 
 def run_module():
@@ -47,7 +150,7 @@ def run_module():
         lanowner=dict(type='str', required=False, default='SYSTEM'),
         vlanid=dict(type='str', required=False),
         macid=dict(type='str', required=False),
-        exists=dict(type='bool', required=False)
+        exists=dict(type='bool', required=False, default='True')
     )
 
     result = dict(
@@ -69,10 +172,10 @@ def run_module():
     cmd_string = ""
     cmd_string += module.params['devno']
 
-    if module.params['exists'] == True:
+    if module.params['exists']:
         cmd_string += " TYPE " + str(module.params['nictype'])
         if module.params['lanname'] is not None:
-            cmd_string += " LAN " + module.params['lanowner'] + ' ' + module.params['lanname'] 
+            cmd_string += " LAN " + module.params['lanowner'] + ' ' + module.params['lanname']
         if module.params['vlanid'] is not None:
             cmd_string += " VLAN " + module.params['vlanid']
         if module.params['macid'] is not None:
@@ -80,11 +183,11 @@ def run_module():
     else:
         cmd_string += " DELETE"
 
-    smcli_results = call_client( module.params['zvmhost'],  \
-                                 module.params['port'],     \
-                                 module.params['authuser'], \
-                                 module.params['authpass'], \
-                                 module.params['name'], "Dirm_Nicdef", cmd_string)
+    smcli_results = call_client(module.params['zvmhost'],
+                                module.params['port'],
+                                module.params['authuser'],
+                                module.params['authpass'],
+                                module.params['name'], "Dirm_Nicdef", cmd_string)
     result['return_code'] = smcli_results[0]
     result['reason_code'] = smcli_results[1]
     result['return_stdout'] = smcli_results[2]
